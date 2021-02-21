05f30f0621ed9265d5e656d2b50c1690ee957984
Darji <mirotvorez00@gmail.com>
2021-02-21 12:15:17 +0300

    1

--- algorithms/fib.py
+++ algorithms/fib.py
@@ -1,8 +1,8 @@
 def fib(n):
-    F = [0, 1] + [None]*(n-1)
-    for i in range(2, n+1):
-        F[i] = F[i-1] + F[i-2]
-    return F[n]
+    f1, f2 = 0, 1
+    for _ in range(n - 1):
+        f1, f2 = f2, f1 + f2
+    return f2
 
 
 if __name__ == '__main__':
